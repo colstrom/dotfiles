7cbeb9d5a6d8720354dc952afea3818d7a0b98ac
Chris Olstrom <chris@olstrom.com>
2015-08-11 17:00:19 -0700

    .config/fish/config.fish: Fix path to z.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
-source ~/.config/fish/plugins/z.fish
+source ~/.config/fish/plugins/z/z.fish
 source /usr/local/share/chruby/chruby.fish
 source /usr/local/share/chruby/auto.fish
 source ~/.config/fish/environment.d/*.fish
