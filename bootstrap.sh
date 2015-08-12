fc50445d719d76f50fa8efc74d8141b111ec7001
Chris Olstrom <chris@olstrom.com>
2015-08-12 12:24:14 -0700

    bootstrap.sh: Delete

--- bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-if [ ! -d ~/ansible ]; then
-  git clone https://github.com/ansible/ansible.git ~/ansible
-fi
-
-source ~/ansible/hacking/env-setup > /dev/null
-ansible-playbook -i ansible_hosts setup.yml
